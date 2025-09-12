b15bef205f6761b0e951b06691b66065bc5d1644
md gulam noxbondy <gulamnoxbondy@gmail.com>
2025-09-12 17:25:51 +0200

    Your commit message

--- src/main/java/se/lexicon/skalmansfoodsleepclock/controller/HomeController.java
+++ src/main/java/se/lexicon/skalmansfoodsleepclock/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/api/health")
     public String home() {
         return "Backend is running 🚀";
     }
