9ac8d71ecf51df50d0f19f5b065d9b7b4c939d97
md gulam noxbondy <gulamnoxbondy@gmail.com>
2025-09-12 22:42:30 +0200

    Fix datasource config for Render

--- src/main/java/se/lexicon/skalmansfoodsleepclock/config/SecurityConfig.java
+++ src/main/java/se/lexicon/skalmansfoodsleepclock/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*");
             }
